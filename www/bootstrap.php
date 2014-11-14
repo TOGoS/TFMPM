09adb847b568bc417baa123f942b9a69a1835fb1
Dan Stevens <stevens@earthit.com>
2014-11-14 15:05:15 -0600

    bootstrap.php can now be used as a router script for PHP's built-in web server.

--- www/bootstrap.php
+++ www/bootstrap.php
@@ -14,6 +14,22 @@ require '../init-www-error-handling.php';
 
 $registry = require '../init-environment.php';
 $dispatcher = $registry->getComponent('PHPTemplateProjectNS_Dispatcher');
-$response = $dispatcher->handleImplicitRequest( $_SERVER['PATH_INFO'] );
+
+if( isset($_SERVER['PATH_INFO']) ) {
+	// bootstrap.php/yaddah-yaddah
+	$path = $_SERVER['PATH_INFO'];
+	$bubble404s = false;
+} else {
+	// php -S ... bootstrap.php
+	$path = $_SERVER['REQUEST_URI'];
+	$bubble404s = true;
+}
+
+$response = $dispatcher->handleImplicitRequest( $path );
+
+// If we're being called by PHP's built-in web server and we
+// don't know about some resource, return false to indicate
+// to the server that it should go look for other files in www/
+if( $bubble404s and $response->getStatusCode() == 404 ) return false;
 
 Nife_Util::outputResponse( $response );
