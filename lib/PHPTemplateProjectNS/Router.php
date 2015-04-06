8adc1fe6b81012f2b6eac93345e4075e2aaa7b5d
Dan Stevens <stevens@earthit.com>
2015-04-06 11:24:22 -0500

    Start refactoring Router to create/invoke actions as separate steps.

--- lib/PHPTemplateProjectNS/Router.php
+++ lib/PHPTemplateProjectNS/Router.php
@@ -54,30 +54,45 @@ class PHPTemplateProjectNS_Router extends PHPTemplateProjectNS_Component
 		return call_user_func( call_user_func_array(array($this,'createPageAction'), func_get_args()) );
 	}
 	
-	public function handleRequest( PHPTemplateProjectNS_RequestContext $ctx ) {
-		$path = $ctx->getPathInfo();
-		// Some demonstration routes; remove and replace with your own
-		if( $path == '/' ) {
-			return $this->doPageAction('ShowHello');
-		} else if( preg_match('<^/uri-res(/.*)>', $path, $bif) ) {
-			return $this->n2rServer->handleRequest($bif[1]);
-		} else if( preg_match('<^/hello/(.*)$>', $path, $matchData) ) {
-			return Nife_Util::httpResponse( 200, "Hello, ".rawurldecode($matchData[1]).'!' );
-		} else if( $path == '/error' ) {
-			trigger_error( "An error occurred for demonstrative porpoises.", E_USER_ERROR );
-		} else if( $path == '/exception' ) {
-			throw new Exception( "You asked for an exception and this is it." );
-		} else if(
-			preg_match('#^/api([;/].*)#',$path,$bif) and
-			($response = $this->handleApiRequest(
-				$ctx->getRequestMethod(),
-				$bif[1], $ctx->getParams(),
-				$ctx->getRequestContentObject())
-			) !== null
-		) {
-			return $response;
+	public function requestToAction( PHPTemplateProjectNS_RequestContext $ctx ) {
+		return function() use ($ctx) {
+			$path = $ctx->getPathInfo();
+			// Some demonstration routes; remove and replace with your own
+			if( $path == '/' ) {
+				return $this->doPageAction('ShowHello');
+			} else if( preg_match('<^/uri-res(/.*)>', $path, $bif) ) {
+				return $this->n2rServer->handleRequest($bif[1]);
+			} else if( preg_match('<^/hello/(.*)$>', $path, $matchData) ) {
+				return Nife_Util::httpResponse( 200, "Hello, ".rawurldecode($matchData[1]).'!' );
+			} else if( $path == '/error' ) {
+				trigger_error( "An error occurred for demonstrative porpoises.", E_USER_ERROR );
+			} else if( $path == '/exception' ) {
+				throw new Exception( "You asked for an exception and this is it." );
+			} else if(
+				preg_match('#^/api([;/].*)#',$path,$bif) and
+				($response = $this->handleApiRequest(
+					$ctx->getRequestMethod(),
+					$bif[1], $ctx->getParams(),
+					$ctx->getRequestContentObject())
+				) !== null
+			) {
+				return $response;
+			} else {
+				return Nife_Util::httpResponse( 404, "I don't know about $path!" );
+			}
+		};
+	}
+	
+	public function doAction($action) {
+		if( is_callable($action) ) {
+			return call_user_func($action, ['this is the context']);
 		} else {
-			return Nife_Util::httpResponse( 404, "I don't know about $path!" );
+			throw new Exception("I don't know how to do ".gettype($act)." as an action");
 		}
+	}
+	
+	public function handleRequest( PHPTemplateProjectNS_RequestContext $ctx ) {
+		$action = $this->requestToAction($ctx);
+		return $this->doAction($action);
 	}
 }
