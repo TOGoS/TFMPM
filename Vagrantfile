ea7b2fe2d16b29174818171d32d0cef325b23bdf
Dan Stevens <stevens@earthit.com>
2015-11-13 10:56:06 -0600

    Add mostly-working Vagrant stuff.

--- Vagrantfile
+++ Vagrantfile
@@ -2,24 +2,12 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-
-  # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "precise32"
+  config.vm.box = "ubuntu/trusty32"
   
-  # The url from where the 'config.vm.box' box will be fetched if it
-  # doesn't already exist on the user's system.
-  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-
-  # Enable the Puppet provisioner, with will look in manifests
-  config.vm.provision :puppet do |puppet|
-    puppet.manifests_path = "puppet/manifests"
-    puppet.manifest_file = "default.pp"
-    puppet.module_path = "puppet/modules"
-  end
-
-  config.vm.synced_folder ".", "/vagrant", :owner => "www-data", :mount_options => ["dmode=777","fmode=777", "umask=0000","dmask=0000","fmask=0000"]
+  config.vm.provision :shell, path: "provision.sh"
+  
+  config.vm.synced_folder ".", "/vagrant", :owner => "www-data", :mount_options => [
+    "dmode=777","fmode=777", "umask=0000","dmask=0000","fmask=0000"]
+  
   config.vm.network :private_network, ip: "192.168.250.249"
-  # config.vm.network "forwarded_port", guest: 80, host: 80
-  # config.vm.network "forwarded_port", guest: 22, host: 22
-  # config.vm.network "forwarded_port", guest: 443, host: 443
 end
---
+++ provision.sh
@@ -0,0 +1,61 @@
+# Used to provision!
+
+set -euo pipefail
+
+#### Set up Postgres
+
+postgres_password='REJ#%*OfdaklJ*O4t5eH'
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list
+
+apt-get update -y
+apt-get install -y postgresql-9.3 postgresql-contrib postgresql-9.3-postgis-2.1
+
+service postgresql start
+
+sed -i 's/127.0.0.1\/32/0.0.0.0\/0/g' /etc/postgresql/9.3/main/pg_hba.conf
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.3/main/postgresql.conf
+
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password '${postgres_password}';"
+
+
+apt-get install -y curl wget vim make openjdk-7-jdk git zip emacs24-nox screen
+
+
+#### Set up Apache (and install PHP while we're at it)
+
+apt-get install -y apache2 \
+    php5 php5-cli php5-curl php5-mcrypt php5-dev php5-pgsql php5-gd libapache2-mod-php5 php5-tidy \
+    python-software-properties
+
+a2enmod rewrite
+#ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
+a2enmod ssl
+
+mkdir -p /etc/apache2/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/apache2/ssl/ssl.key \
+    -out /etc/apache2/ssl/ssl.crt \
+    -subj "/C=US/ST=Wisconsin/L=Madison/O=Earthling Interactive/OU=Dev/CN=hello"
+
+cp /vagrant/vm-files/vhost /etc/apache2/sites-available/000-default.conf
+chmod 0600 /etc/apache2/sites-available/000-default.conf
+
+
+#### Fix some PHP INI settings
+
+sed -i \
+    -e 's/upload_max_filesize = [0-9]\+M/upload_max_filesize = 100M/g' \
+    -e 's/post_max_size = [0-9]\+M/post_max_size = 110M/g' \
+    -e 's/; max_input_vars = [0-9]\+/max_input_vars = 1000000/g' \
+    /etc/php5/apache2/php.ini
+
+wget https://getcomposer.org/installer -O - | php
+mv composer.phar /usr/local/bin/composer
+
+
+
+#### Start apache
+
+service apache2 start
