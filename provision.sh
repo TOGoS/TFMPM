45101b3d0998e22ad6abd785da4568a181f1e93d
Dan Stevens <stevens@earthit.com>
2015-11-13 11:04:26 -0600

    Tweaks to make provisioning script less noisy.

--- provision.sh
+++ provision.sh
@@ -2,12 +2,22 @@
 
 set -euo pipefail
 
+
+#### Install some packages needed by later steps
+
+apt-get install -y wget
+
+
 #### Set up Postgres
 
 postgres_password='REJ#%*OfdaklJ*O4t5eH'
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list
+wget -q -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+cat \
+    /etc/apt/sources.list.d/postgresql.list \
+    <(echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main") \
+    | uniq > /tmp/postgresql.list
+mv /tmp/postgresql.list /etc/apt/sources.list.d/postgresql.list
 
 apt-get update -y
 apt-get install -y postgresql-9.3 postgresql-contrib postgresql-9.3-postgis-2.1
@@ -20,7 +30,7 @@ sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgre
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password '${postgres_password}';"
 
 
-apt-get install -y curl wget vim make openjdk-7-jdk git zip emacs24-nox screen
+apt-get install -y vim make openjdk-7-jdk git zip emacs24-nox screen
 
 
 #### Set up Apache (and install PHP while we're at it)
@@ -37,7 +47,8 @@ mkdir -p /etc/apache2/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/apache2/ssl/ssl.key \
     -out /etc/apache2/ssl/ssl.crt \
-    -subj "/C=US/ST=Wisconsin/L=Madison/O=Earthling Interactive/OU=Dev/CN=hello"
+    -subj "/C=US/ST=Wisconsin/L=Madison/O=Earthling Interactive/OU=Dev/CN=hello" \
+    2>/dev/null # Otherwise it is tooo noisy!
 
 cp /vagrant/vm-files/vhost /etc/apache2/sites-available/000-default.conf
 chmod 0600 /etc/apache2/sites-available/000-default.conf
@@ -51,7 +62,7 @@ sed -i \
     -e 's/; max_input_vars = [0-9]\+/max_input_vars = 1000000/g' \
     /etc/php5/apache2/php.ini
 
-wget https://getcomposer.org/installer -O - | php
+wget -q 'https://getcomposer.org/installer' -O - | php
 mv composer.phar /usr/local/bin/composer
 
 
