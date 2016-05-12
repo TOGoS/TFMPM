70e0ac1c6da63c3e9a8e7807ebe6427b3dcee656
Dan Stevens <stevens@earthit.com>
2016-05-12 11:33:22 -0400

    Schema updates and upgrade script for postal addresses.

--- build/db/upgrades/0110-create-tables.sql
+++ build/db/upgrades/0110-create-tables.sql
@@ -5,10 +5,26 @@ CREATE TABLE "phptemplateprojectdatabasenamespace"."user" (
 	"emailaddress" VARCHAR(126),
 	PRIMARY KEY ("id")
 );
+CREATE TABLE "phptemplateprojectdatabasenamespace"."postaladdress" (
+	"id" CHAR(32) NOT NULL,
+	"streetaddress" VARCHAR(126),
+	"unitaddress" VARCHAR(126),
+	"cityname" VARCHAR(126),
+	"regioncode" CHAR(2),
+	"postalcode" VARCHAR(126),
+	"countrycode" CHAR(3),
+	PRIMARY KEY ("id")
+);
 CREATE TABLE "phptemplateprojectdatabasenamespace"."organization" (
 	"id" BIGINT NOT NULL DEFAULT nextval('phptemplateprojectdatabasenamespace.newentityid'),
 	"name" VARCHAR(126) NOT NULL,
 	PRIMARY KEY ("id")
+);
+CREATE TABLE "phptemplateprojectdatabasenamespace"."entitypostaladdress" (
+	"entityid" BIGINT NOT NULL,
+	"postaladdressid" CHAR(32) NOT NULL,
+	PRIMARY KEY ("entityid", "postaladdressid"),
+	FOREIGN KEY ("postaladdressid") REFERENCES "phptemplateprojectdatabasenamespace"."postaladdress" ("id")
 );
 CREATE TABLE "phptemplateprojectdatabasenamespace"."userorganizationattachment" (
 	"userid" BIGINT NOT NULL,
@@ -27,4 +43,23 @@ CREATE TABLE "phptemplateprojectdatabasenamespace"."computation" (
 	"result" VARCHAR(126),
 	PRIMARY KEY ("expression"),
 	FOREIGN KEY ("statuscode") REFERENCES "phptemplateprojectdatabasenamespace"."computationstatus" ("statuscode")
+);
+CREATE TABLE "phptemplateprojectdatabasenamespace"."product" (
+	"id" BIGINT NOT NULL DEFAULT nextval('phptemplateprojectdatabasenamespace.newentityid'),
+	"upc" VARCHAR(126) NOT NULL,
+	"title" VARCHAR(126) NOT NULL,
+	"descriptionhtml" TEXT NOT NULL,
+	PRIMARY KEY ("id")
+);
+CREATE TABLE "phptemplateprojectdatabasenamespace"."order" (
+	"id" BIGINT NOT NULL DEFAULT nextval('phptemplateprojectdatabasenamespace.newentityid'),
+	"userid" BIGINT NOT NULL,
+	PRIMARY KEY ("id"),
+	FOREIGN KEY ("userid") REFERENCES "phptemplateprojectdatabasenamespace"."user" ("id")
+);
+CREATE TABLE "phptemplateprojectdatabasenamespace"."orderitem" (
+	"orderid" BIGINT NOT NULL,
+	"productid" BIGINT NOT NULL,
+	FOREIGN KEY ("orderid") REFERENCES "phptemplateprojectdatabasenamespace"."order" ("id"),
+	FOREIGN KEY ("productid") REFERENCES "phptemplateprojectdatabasenamespace"."product" ("id")
 );
