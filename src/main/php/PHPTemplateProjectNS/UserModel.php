fb7d6372cc4f5017c08b412151726f24ba38b3fc
Dan Stevens <stevens@earthit.com>
2016-12-01 10:53:51 -0600

    Move PASSWORD_CHARS, generatePassword from UserModel to UserModelTest, since that's the only place that actually uses those.

--- src/main/php/PHPTemplateProjectNS/UserModel.php
+++ src/main/php/PHPTemplateProjectNS/UserModel.php
@@ -2,18 +2,6 @@
 
 class PHPTemplateProjectNS_UserModel extends PHPTemplateProjectNS_Component
 {
-	const PASSWORD_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-	
-	public function generatePassword( $length=20 ) {
-		$rm = strlen(self::PASSWORD_CHARS)-1;
-		$pw = '';
-		for( $i=0; $i<$length; ++$i ) {
-			$idx = mt_rand(0, $rm);
-			$pw .= substr(self::PASSWORD_CHARS, $idx, 1);
-		}
-		return $pw;
-	}
-	
 	/**
 	 * @return array( 'success' => true|false, 'message'=>'Failed to log in because you are too tall', 'userId'=>123 )
 	 */
