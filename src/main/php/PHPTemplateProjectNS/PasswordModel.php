15f46bd2804e1af18f13289da4789c3cab7b9cd5
Dan Stevens <stevens@earthit.com>
2016-12-01 11:00:33 -0600

    Replace hash_hmac('sha1', ...) with password_hash.

--- src/main/php/PHPTemplateProjectNS/PasswordModel.php
+++ src/main/php/PHPTemplateProjectNS/PasswordModel.php
@@ -3,18 +3,12 @@
 class PHPTemplateProjectNS_PasswordModel extends PHPTemplateProjectNS_Component
 {
 	public function hashPassword($password) {
-		// TODO: Use that more recommended algorithm instead of hash_hmac.
-		// http://php.net/manual/en/book.password.php
-		$salt = mt_rand()."-".mt_rand();
-		return $salt.':'.hash_hmac('sha1', $password, $salt);
+		return hash_password($password, PASSWORD_BCRYPT, array(
+			'cost' => 15,
+		));
 	}
 	
 	public function checkPassword($password, $passhash) {
-		$parts = explode(':',$passhash,2);
-		if( count($parts) != 2 ) {
-			return false;
-		}
-		list($salt,$hash) = $parts;
-		return hash_hmac('sha1', $password, $salt) === $hash;
+		return password_verify($password, $passhash);
 	}
 }
